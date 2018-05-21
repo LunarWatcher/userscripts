3af639c57548b546f3fae538ec988c922db4bd0b
Olivia Zoe <zoe.i2k1@gmail.com>
2018-05-21 14:40:31 +0200

    Now with tag replacements

--- Tag removal.user.js
+++ Tag removal.user.js
@@ -1,20 +1,26 @@
 // ==UserScript==
-// @name         Tag removal
+// @name         Retagger
 // @namespace    https://github.com/LunarWatcher/userscripts
-// @version      1.0.1
-// @description  Easy tag burnination or removal
+// @version      1.0.2
+// @description  Easy tag burnination, removal, and retagging
 // @author       Olivia Zoe
 // @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
 // @grant        none
 // ==/UserScript==
 
 var reasons = {
-    "ide": "IDE tags should not be used for questions that aren't about the IDE itself"
+    "ide": "IDE tags should not be used for questions that aren't about the IDE itself",
+    "genericBurnination": "Removed tag that's in the process of burnination; see meta"
 };
 var tagTargets = {
     "adobe": "Removed the adobe tag as per https://graphicdesign.meta.stackexchange.com/questions/3455/project-bye-bye-adobe-tag",
     "android-studio": reasons["ide"],
     "intellij": reasons["ide"]
+};
+
+//Format: "tag name": ["tag replacement", "Replacement reason"]
+var tagReplacements = {
+    "checkout" : ["vcs-checkout", "Checkout is used for questions about finishing transactions, not the version control system feature \"checkout\""]
 };
 
 (function() {
@@ -65,13 +71,24 @@ function clearTags(){
 
     if(tags !== undefined){
         var keys = Object.keys(tagTargets);
-        outer: for (var i in keys) for(var j = tags.length - 1; j >= 0; j--){
+        for (var i in keys) for(var j = tags.length - 1; j >= 0; j--){
             var key = keys[i];
             console.log("Checking for the tag " + key);
             if(tags[j].textContent === key){
                 tags[j].children[0].click();
                 editDetails.val(tagTargets[key] + "; ");
                 tags = $(".post-tag");
+            }
+        }
+        keys = Object.keys(tagReplacements);
+        for( var n in keys) for(var m = tags.length - 1; m >= 0; m--){
+            var nKey = keys[n];
+            console.log("Checking for the tag " + nKey);
+            if(tags[m].textContent === nKey){
+                tags[m].children[0].click();
+                editDetails.val(tagReplacements[nKey][1] + "; ");
+                $(".tag-editor").find("input").val(tagReplacements[nKey][0]);
+                tags = $(".post-tag.rendered-element");
             }
         }
     }
