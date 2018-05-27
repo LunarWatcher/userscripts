18c6bda5b85bb10fcd58fa44e0ee4cf49c34c799
Olivia Zoe <zoe.i2k1@gmail.com>
2018-05-27 21:05:30 +0200

    Update Tag removal.user.js

--- Tag removal.user.js
+++ Tag removal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Retagger
 // @namespace    https://github.com/LunarWatcher/userscripts
-// @version      1.0.2
+// @version      1.0.3
 // @description  Easy tag burnination, removal, and retagging
 // @author       Olivia Zoe
 // @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
@@ -15,7 +15,8 @@ var reasons = {
 var tagTargets = {
     "adobe": "Removed the adobe tag as per https://graphicdesign.meta.stackexchange.com/questions/3455/project-bye-bye-adobe-tag",
     "android-studio": reasons["ide"],
-    "intellij": reasons["ide"]
+    "intellij": reasons["ide"],
+    "intellij-idea": reasons["ide"]
 };
 
 //Format: "tag name": ["tag replacement", "Replacement reason"]
