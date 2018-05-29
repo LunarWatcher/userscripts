3308bbab6483d54ab27533014e9d418cf519abf4
Olivia Zoe <zoe.i2k1@gmail.com>
2018-05-29 08:38:19 +0200

    Update and rename Tag removal.user.js to TagRemoval.user.js

--- TagRemoval.user.js
+++ TagRemoval.user.js
@@ -6,6 +6,8 @@
 // @author       Olivia Zoe
 // @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
 // @grant        none
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/TagRemoval.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/TagRemoval.meta.js
 // ==/UserScript==
 
 var reasons = {
