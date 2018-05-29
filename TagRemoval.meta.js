de41b5930f7dbd1973e1fb36f018840b1c37013e
Olivia Zoe <zoe.i2k1@gmail.com>
2018-05-29 08:38:57 +0200

    This should work

---
+++ TagRemoval.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Retagger
+// @namespace    https://github.com/LunarWatcher/userscripts
+// @version      1.0.3
+// @description  Easy tag burnination, removal, and retagging
+// @author       Olivia Zoe
+// @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
+// @grant        none
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/TagRemoval.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/TagRemoval.meta.js
+// ==/UserScript==
