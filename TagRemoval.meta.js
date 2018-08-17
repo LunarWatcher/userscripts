3582fdc1f01d870d8cf1566110221e7520ad2468
Olivia Zoe <zoe.i2k1@gmail.com>
2018-08-17 14:29:37 +0200

    Update TagRemoval.meta.js

--- TagRemoval.meta.js
+++ TagRemoval.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Retagger
 // @namespace    https://github.com/LunarWatcher/userscripts
-// @version      1.0.5
+// @version      1.0.8
 // @description  Easy tag burnination, removal, and retagging
 // @author       Olivia Zoe
 // @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
