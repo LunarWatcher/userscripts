5d547e4f0aadf4becefa48d362bc9ca11d00092e
Olivia Zoe <zoe.i2k1@gmail.com>
2018-08-18 17:00:14 +0200

    Create IHateUnderlines.user.js

---
+++ IHateUnderlines.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         I hate underlines!
+// @namespace    https://github.com/LunarWatcher/userscripts
+// @version      1.0.0
+// @description  Removes underlines from links in the SE network.
+// @author       Olivia Zoe
+// @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
+// @grant        none
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/IHateUnderlines.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/IHateUnderlines.meta.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    /**
+     * Works straight forward: injects some CSS that forces removal of text decoration
+     */
+    var cssElement = document.createElement("style");
+    cssElement.type = "text/css";
+    cssElement.innerHTML =
+        "a { " +
+        "    text-decoration: none !important;" +
+        "}";
+    document.body.appendChild(cssElement);
+})();
