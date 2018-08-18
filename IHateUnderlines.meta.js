84838bac6b718c11e8bc9de8740f642810bf7d92
Olivia Zoe <zoe.i2k1@gmail.com>
2018-08-18 17:00:29 +0200

    Create IHateUnderlines.meta.js

---
+++ IHateUnderlines.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         I hate underlines!
+// @namespace    https://github.com/LunarWatcher/userscripts
+// @version      1.0.0
+// @description  Removes underlines from links in the SE network.
+// @author       Olivia Zoe
+// @include      /^https?:\/\/\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com\/(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
+// @grant        none
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/IHateUnderlines.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/IHateUnderlines.meta.js
+// ==/UserScript==
