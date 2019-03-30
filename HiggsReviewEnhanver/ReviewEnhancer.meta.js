a123b82fd8bd012c971fb2a6d1f679f73db1c95e
Olivia Zoe <zoe.i2k1@gmail.com>
2019-03-30 14:42:33 +0100

    Create ReviewEnhancer.meta.js

---
+++ HiggsReviewEnhanver/ReviewEnhancer.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Higgs review enhancer
+// @namespace    https://github.com/LunarWatcher/userscripts
+// @version      1.0.0
+// @include      https://higgs.sobotics.org/review
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/HiggsReviewEnhancer/ReviewEnhancer.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/HiggsReviewEnhancer/ReviewEnhancer.meta.js
+// @author       Olivia Zoe
+// ==/UserScript==
