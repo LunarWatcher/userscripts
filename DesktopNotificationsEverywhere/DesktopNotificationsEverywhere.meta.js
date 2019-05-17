4a605fd967385f42edf4d9d563308eb8ea07b6dc
Olivia Zoe <zoe.i2k1@gmail.com>
2019-05-17 15:58:55 +0200

    Create DesktopNotificationsEverywhere.meta.js

---
+++ DesktopNotificationsEverywhere/DesktopNotificationsEverywhere.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Desktop Notifications Everywhere
+// @namespace    https://github.com/LunarWatcher/userscripts
+// @version      1.0.0
+// @description  Sends desktop notifications for every message sent
+// @author       Olivia Zoe
+// @match        *://chat.stackexchange.com/rooms/*
+// @match        *://chat.meta.stackexchange.com/rooms/*
+// @match        *://chat.stackoverflow.com/rooms/*
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/DesktopNotificationsEverywhere/DesktopNotificationsEverywhere.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/DesktopNotificationsEverywhere/DesktopNotificationsEverywhere.meta.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
