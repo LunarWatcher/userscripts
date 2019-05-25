ae35f75be491ee33fe931820479c01259d9b0639
Olivia Zoe <zoe.i2k1@gmail.com>
2019-05-25 15:37:57 +0200

    Create MagicEditorURLMod.meta.js

---
+++ MagicEditor/MagicEditorURLMod.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name           Magic™Editor
+// @author         Cameron Bernhardt (AstroCB)
+// @developer      Jonathan Todd (jt0dd)
+// @developer      sathyabhat
+// @contributor    Unihedron
+// @contributor    Tiny Giant
+// @contributor    Mogsdad
+// @contributor    Makyen
+// @grant          none
+// @license        MIT
+// @namespace      http://github.com/LunarWatcher/userscripts/MagicEditor
+// @version        1.7.0.0
+// @description    Fix common grammar/usage annoyances on Stack Exchange posts with a click
+//                 Forked from https://github.com/AstroCB/Stack-Exchange-Editor-Toolkit
+// @include        /^https?:\/\/([\w-]*\.)*((stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com|mathoverflow.net)\/(c\/[^\/]*\/)?(questions|posts|review|tools)\/(?!tagged\/|new\/).*/
+// @exclude       *://chat.stackoverflow.com/*
+// @exclude       *://chat.stackexchange.com/*
+// @exclude       *://chat.*.stackexchange.com/*
+// @exclude       *://api.*.stackexchange.com/*
+// @exclude       *://data.stackexchange.com/*
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/MagicEditor/MagicEditorURLMod.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/MagicEditor/MagicEditorURLMod.meta.js
+// ==/UserScript==
