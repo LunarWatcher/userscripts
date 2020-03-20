0df53e885581d102e2d03acced924dabd22ca266
Olivia Zoe <zoe.i2k1@gmail.com>
2020-03-20 10:42:18 +0100

    Create IWantBackgroundsOnBlockquotes.stack.meta.js

---
+++ Blockquotes/IWantBackgroundsOnBlockquotes.stack.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Bring back blockquotes with a background
+// @namespace    https://github.com/lunarwatcher/userscripts
+// @version      1.0
+// @description  try to take over the world!
+// @author       Olivia Zoe
+// @match        *://*.stackexchange.com/*
+// @match        *://*.stackoverflow.com/*
+// @match        *://*.superuser.com/*
+// @match        *://*.serverfault.com/*
+// @match        *://*.askubuntu.com/*
+// @match        *://*.stackapps.com/*
+// @match        *://*.mathoverflow.net/*
+// @match      *://*.area51.stackexchange.com/*
+// @exclude      *://chat.stackexchange.com/*
+// @exclude      *://chat.meta.stackexchange.com/*
+// @exclude      *://chat.stackoverflow.com/*
+// @exclude      *://blog.stackoverflow.com/*
+// @grant        none
+// @downloadURL  https://github.com/LunarWatcher/userscripts/raw/master/Blockquotes/IWantBackgroundsOnBlockquotes.stack.user.js
+// @updateURL    https://github.com/LunarWatcher/userscripts/raw/master/Blockquotes/IWantBackgroundsOnBlockquotes.stack.meta.js
+// ==/UserScript==
