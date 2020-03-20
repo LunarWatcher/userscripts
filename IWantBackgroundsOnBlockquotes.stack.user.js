61ddddbbba9b8715539d00261ad4dcdc232a6ea0
Olivia Zoe <zoe.i2k1@gmail.com>
2020-03-20 10:39:11 +0100

    Create IWantBackgroundsOnBlockquotes.stack.user.js

---
+++ IWantBackgroundsOnBlockquotes.stack.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Bring back blockquotes with a background
+// @namespace    https://github.com/lunarwatcher/userscripts
+// @version      1.0
+// @description  try to take over the world!
+// @author       Olivia Zoe
+// @match        *://*.stackexchange.com/*
+// @match        *://*.stackoverflow.com/*
+// @match        *://*.superuser.com/*
+// @match        *://*.serverfault.com/*
+// @match        *://*.askubuntu.com/*
+// @match        *://*.stackapps.com/*
+// @match        *://*.mathoverflow.net/*
+// @match      *://*.area51.stackexchange.com/*
+// @exclude      *://chat.stackexchange.com/*
+// @exclude      *://chat.meta.stackexchange.com/*
+// @exclude      *://chat.stackoverflow.com/*
+// @exclude      *://blog.stackoverflow.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let css =`
+blockquote {
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: #fbf2d4 !important;
+    border-left: 2px solid #ffeb8e !important;
+}
+
+blockquote::before {
+    background: none !important; /*change to the value of border-left for a border with the same style as the new quotes. Setting this to none disables it and entirely reverts votes*/
+}
+`;
+
+    let styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = css;
+    document.head.appendChild(styleSheet);
+
+})();
